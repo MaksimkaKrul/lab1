697c6eb4382e38ad493cc80af457a8d8683f91e1
MaksimkaKrul <krulevckiym@gmail.com>
2025-02-21 16:40:46 +0200

    Updated implementation for solving quadratic equalations.

--- solver.py
+++ solver.py
@@ -1,26 +1,29 @@
-import sys, math
+import sys, math, cmath
 
 def solve_quadratic(a: float, b: float, c: float):
     if a == 0:
         print("Error. a cannot be 0", file=sys.stdout)
         sys.exit(1)
-    
+
     discriminant = (b ** 2) - (4 * a * c)
-    squared_d = math.sqrt(discriminant)
-    
+
     print(f"Solving equation: {a}x^2 + {b}x + {c} = 0")
-    
-    if squared_d > 0:
-        x1 = (-b + (math.sqrt(squared_d))) / (2 * a)
-        x2 = (-b - (math.sqrt(squared_d))) / (2 * a)
+
+    if discriminant > 0:
+        squared_d = math.sqrt(discriminant)
+        x1 = (-b + squared_d) / (2 * a)
+        x2 = (-b - squared_d) / (2 * a)
         print("There are 2 real roots.")
         print(f"x1 = {x1}, x2 = {x2}")
 
-    elif squared_d == 0:
+    elif discriminant == 0:
         x = (-b) / (2 * a)
         print("There is exactly 1 real root.")
         print(f"x = {x}")
 
     else:
-        print("No real roots exist for this equation.")
-
+        squared_d = cmath.sqrt(discriminant)
+        x1 = (-b + squared_d) / (2 * a)
+        x2 = (-b - squared_d) / (2 * a)
+        print("There are 2 complex roots:")
+        print(f"x1 = {x1}, x2 = {x2}")
